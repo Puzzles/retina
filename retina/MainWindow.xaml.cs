4cde1e6689bdc17b771cf7cb7e7fa7b00cbd6efa
Andrew Hobden <andy.hobden@gmail.com>
2011-09-09 11:34:15 -0700

    Added basic button logic as well as basic tab switching (Though the tabs aren't populated or coded to spawn/destroy)

--- retina/MainWindow.xaml.cs
+++ retina/MainWindow.xaml.cs
@@ -1,5 +1,5 @@
-﻿// This is fully testing VS2010's git capabilities. (Okay not really)
-
+﻿/* Retina is a WPF based IRC client currently in development.
+ */
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +24,39 @@ namespace retina
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.messageBox.Text = this.messageBox.Text + "\n" + sendBox.Text;
+            sendBox.Clear();
+            
+        }
+
+        private void sendBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Do spellcheck logic?
+        }
+
+        private void memberList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Do hover-over logic? (Ident information?)
+        }
+
+        private void channelTabs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Do change-tab logic.
+        }
+
+        private void editMenu_Click(object sender, RoutedEventArgs e)
+        {
+            // Do edit menu logic.
+        }
+
+        private void preferencesMenu_Click(object sender, RoutedEventArgs e)
+        {
+            // Do preferences menu logic.
         }
     }
 }
